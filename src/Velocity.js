933c6ab1ba1d9fa473fdf14787947b4ef8832ed5
Arunadevi <l.arunadevi@gmail.com>
2022-07-29 18:52:37 +0530

    Direction

--- src/Velocity.js
+++ src/Velocity.js
@@ -14,7 +14,26 @@ define(function (require) {
     const pos2 = new Position(p2.latitude, p2.longitude, p2.timestamp);
     const distance = pos2.getDistanceFrom(pos1);
     const timeTaken = (pos2.timestamp - pos1.timestamp) / (60 * 60);
-    return distance / timeTaken;
+
+    const radiansToDegree = (latLong) => {
+      return (latLong * 180.0 / Math.PI);
+    }
+
+    const lng2 = p2.longitude;
+    const lng1 = p1.longitude;
+    const lat1 = p1.latitude;
+    const lat2 = p2.latitude;
+    const dLon = (lng2 - lng1);
+    const y = Math.sin(dLon) * Math.cos(lat2);
+    const x = Math.cos(lat1) * Math.sin(lat2) - Math.sin(lat1) * Math.cos(lat2) * Math.cos(dLon);
+    let brng = radiansToDegree((Math.atan2(y, x)));
+    brng = (360 - ((brng + 360) % 360));
+
+    this.speed = distance / timeTaken;
+    this.direction = brng;
+    this.timestamp = p2.timestamp;
+
+    return this;
   }
   return Velocity;
 });
--- src/main.js
+++ src/main.js
@@ -3,5 +3,7 @@ define(function (require) {
 
     const p1 = {latitude: 123, longitude: 125, timestamp: Math.floor((new Date()).getTime() / 1000)};
     const p2 = {latitude: 123, longitude: 130, timestamp: Math.floor((new Date().getTime() + (1000 * 60 * 60 * 3) )/ 1000)};
-    console.log("Velocity: " + new Velocity().calcVelocity(p1, p2));
+    const velocity = new Velocity().calcVelocity(p1, p2);
+    console.log("Velocity: " + velocity.speed);
+    console.log("Direction: ", velocity.direction);
 });
