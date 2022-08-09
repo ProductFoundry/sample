d5036ecb3a6deac764259d82e23f9b69dca9d40e
Arunadevi <l.arunadevi@gmail.com>
2022-08-09 19:29:04 +0530

    Create entry point

--- src/main.js
+++ src/main.js
@@ -1,46 +1,6 @@
-define(function (require) {
-  const AssetCollection = require('./AssetCollection');
-  const DataFileReader = require('./DataFileReader');
-  const assets = new AssetCollection();
+define('main', ['Manager'], function (Manager) {
   const fileInput = $(".files")
-  fileInput.on('change', (e) => {
-    const { files } = e.target;
-    const f = new DataFileReader(files[0]);
-    const check = function () {
-      const events = f.getEvents();
-      if (events) {
-        events.forEach(event => {
-          const asset = assets.getAsset(event.aId);
-          if (asset) {
-            asset.setCurrentPosition(event.lat, event.long, event.ts);
-          } else {
-            assets.addAsset(event.aId, event.aName, event.aType, event.lat, event.long, event.ts)
-          }
-        });
-        const tbody = $("#assets tbody")
-        assets.getAllAssets().forEach(a => {
-          const tr = $("<tr></tr>")
-          $(tr).append("<td>" + a.name + "</td>")
-            .append("<td>" + new Date(a.currentPosition.timestamp * 1000).toTimeString().split(" ")[0] + "</td>")
-            .append("<td>" + a.currentPosition.latitude + "</td>")
-            .append("<td>" + a.currentPosition.longitude + "</td>")
-            .append("<td>" + a.velocity.speed + "</td>")
-            .append("<td>" + a.velocity.direction + "</td>");
-          $(tbody).append(tr)
-        })
-        return;
-      }
-      setTimeout(check, 1000);
-    }
+  const tbody = $("#assets tbody");
+  const manager = new Manager(fileInput, tbody);
 
-    check();
-
-
-  });
-
-  // const p1 = {latitude: 123, longitude: 125, timestamp: Math.floor((new Date()).getTime() / 1000)};
-  // const p2 = {latitude: 123, longitude: 130, timestamp: Math.floor((new Date().getTime() + (1000 * 60 * 60 * 3) )/ 1000)};
-  // const velocity = new Velocity().calcVelocity(p1, p2);
-  // console.log("Velocity: " + velocity.speed);
-  // console.log("Direction: ", velocity.direction);
 });
