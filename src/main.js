4d421a44c4162137380bf5ebc83343b3a5c6d5fc
Arunadevi <l.arunadevi@gmail.com>
2022-08-16 14:18:48 +0530

    Test comment

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 define('main', ['Manager'], function (Manager) {
   const fileInput = $(".files")
   const tbody = $("#assets tbody");
+  console.log(fileInput, tbody);
   const manager = new Manager(fileInput, tbody);
 
 });
