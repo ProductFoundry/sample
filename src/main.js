002c3c2172778c120ff0c54785227222e5cc1265
ProductFoundry <107924397+ProductFoundry@users.noreply.github.com>
2024-07-05 14:10:37 +0530

    commitTitle

--- src/main.js
+++ src/main.js
@@ -31,7 +31,7 @@ define('main', ['DataFileReader', 'AssetCollection'], function (DataFileReader, 
         })
         return;
       }
-      setTimeout(check, 1001);
+      setTimeout(check, 1002);
     }
 
     check();
