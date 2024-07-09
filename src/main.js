448c44ed243119c48dbe129c6f79a751f7f395fc
ProductFoundry <107924397+ProductFoundry@users.noreply.github.com>
2024-07-09 14:55:46 +0530

    Test commit 2

--- src/main.js
+++ src/main.js
@@ -31,7 +31,7 @@ define('main', ['DataFileReader', 'AssetCollection'], function (DataFileReader, 
         })
         return;
       }
-      setTimeout(check, 1005);
+      setTimeout(check, 1006);
     }
 
     check();
